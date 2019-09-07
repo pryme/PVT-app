8dd981dc13ebcc1c0602d7632007ec4ef1062254
pryme <pryme4gh@gmail.com>
2019-09-07 16:03:24 -0400

    Refactor AddNewUser to functional component.

--- src/add_new_user.js
+++ src/add_new_user.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { valueInLS, addNewUser} from './user_list';
 
-class AddNewUser extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  onSubmit = (e) => {
+function AddNewUser(props) {
+  const onSubmit = (e) => {
     e.preventDefault();
     // e.target is the id="newUser" form
     // e.target.firstName.value gives value of name="firstName" field
@@ -16,21 +12,19 @@ class AddNewUser extends React.Component {
     let isNew = !valueInLS(fullName);
     if (isNew) {addNewUser(fullName);}
     // this callback passes up the name, and also resets AddNew flag
-    this.props.cb(fullName);  
+    props.cb(fullName);  
     //alert("AddNewUser.onSubmit: Go to Comments / Proceed");
   }
+  return(
+    <form onSubmit={onSubmit} id="newUser">
+      <h1>New User Setup</h1>
+      <p>Enter your info below.</p>
+      <input type="text" name="firstName" placeholder="First name" />
+      <input type="text" name="lastName" placeholder="Last name" />
+      <input type="submit" value="Submit" />
+    </form>            
+  );
 
-  render() {
-    return(
-      <form onSubmit={this.onSubmit} id="newUser">
-        <h1>New User Setup</h1>
-        <p>Enter your info below.</p>
-        <input type="text" name="firstName" placeholder="First name" />
-        <input type="text" name="lastName" placeholder="Last name" />
-        <input type="submit" value="Submit" />
-      </form>            
-    );
-  }
 }
 
 export default AddNewUser;
