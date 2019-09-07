6c8e1b5c68dff0b780536cf80b2fdd74fabfe157
pryme <pryme4gh@gmail.com>
2019-09-07 15:09:48 -0400

    Refactor ChooseUser to functional component.

--- src/choose_user.js
+++ src/choose_user.js
@@ -1,62 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddNewUser from './add_new_user';
 
-class ChooseUser extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      msg: "I'm not on the list",
-      addUser: false
-    };
-  }
+function ChooseUser(props) {
+  const msg = "I'm not on the list";
+  const [addUser, setAddUser] = useState(false);
 
-  onSelectSubmit = (e) => {
+  const onSelectSubmit = (e) => {
     e.preventDefault();
     let pick = e.target.list.value;  // name from select
-    if (pick !== this.state.msg) {
-      this.props.cb(pick);  // return selected name upward
+    if (pick !==msg) {
+      props.cb(pick);  // return selected name upward
     } else {  // need to add a new user
-      this.setState({
-        addUser: true  // set flag
-      });  
+      setAddUser(true);  // set flag
     }
   }
-
-  addNewDoneCB = (name) => {   // call this when done adding new user
-    this.setState({
-      addUser: false  // assume future users are existing
-    });
-    this.props.cb(name);
+  const addNewDoneCB = (name) => {   // call this when done adding new user
+    setAddUser(false);  // assume future users are existing
+    props.cb(name);
   }
 
-  render() {
-    const lsKeys = Object.keys(localStorage);
-    if (!(this.state.addUser) && lsKeys.length > 0) {  // first ask to select from list
-      let names = lsKeys.map(   // array of userNames
-          key => (JSON.parse(localStorage.getItem(key)).userName)
-        );
-      return(
-        <div>
-          <h1>Choose User</h1>
-          <form onSubmit={this.onSelectSubmit} >
-            <p>Choose your name from the list:</p>
-              <select name="list">    
-                {names.map(
-                  name => <option>{name}</option>
-                )}
-                <option>{this.state.msg}</option>
-              </select>
-            <p><input type="submit" value="Submit" /></p>
-          </form>
-        </div>
+  const lsKeys = Object.keys(localStorage);
+  if (!(addUser) && lsKeys.length > 0) {  // first ask to select from list
+    let names = lsKeys.map(   // array of userNames
+        key => (JSON.parse(localStorage.getItem(key)).userName)
       );
-    } else {  // need to add new user
-      return(
-        <AddNewUser 
-          cb={this.addNewDoneCB} 
-        />
-      );
-    }
+    return(
+      <div>
+        <h1>Choose User</h1>
+        <form onSubmit={onSelectSubmit} >
+          <p>Choose your name from the list:</p>
+            <select name="list">    
+              {names.map(
+                name => <option>{name}</option>
+              )}
+              <option>{msg}</option>
+            </select>
+          <p><input type="submit" value="Submit" /></p>
+        </form>
+      </div>
+    );
+  } else {  // need to add new user
+    return(
+      <AddNewUser 
+        cb={addNewDoneCB} 
+      />
+    );
   }
 }
 
