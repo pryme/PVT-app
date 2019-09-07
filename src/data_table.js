3f7f3977de4040b0fb01461faf758ac41b9e26ae
pryme <pryme4gh@gmail.com>
2019-09-07 12:43:11 -0400

    Refactor DataTable to functional component.

--- src/data_table.js
+++ src/data_table.js
@@ -1,6 +1,89 @@
 import React from 'react';
 
-class DataTable extends React.Component {
+function DataTable(props) {
+  const generateRows = () => {
+    const results = props.results;
+    const lapseThresh = props.lapseThresh;
+    if (results.length > 0) {
+      return results.map(function(item, index) {
+          let content;
+          let cellClass;
+          if (item === null) {
+            content = "FS"  // false start
+          } else {
+            content = item;
+            if (Number(content) > lapseThresh) {cellClass = "lapse";} 
+          }
+          return <tr key={index}><td className={cellClass}> {content} </td></tr>;
+        });
+    }
+  }
+
+  const toMatrix = (arr, width) => {
+   return arr.reduce(function(rows, val, idx) {
+     return (idx % width === 0 ? rows.push([val]) : 
+       rows[rows.length - 1].push(val)) && rows;
+   }, []);
+  }
+
+  const lapseThresh = props.lapseThresh;
+  const validThresh = props.validThresh;
+  const results = props.results.map(
+    function(item) {
+      let val;
+      item === null ? val = "FS" : val = Number(item);
+      return val;
+    }
+  );
+  // reshape to matrix
+  const cols = 5;
+  const mat = toMatrix(results, cols);  // false starts already labelled
+  try {
+    return <div className="results">
+            <table>
+              <caption>Response Time</caption>
+              <tbody>
+              {mat.map(
+                function(row, idx) {
+                  return <tr key={idx}>
+                    {row.map(function(cell, idx) {
+                      let cellClass;
+                      if (cell === "FS" || cell < validThresh) {
+                        cellClass = "false";
+                      }
+                      if (cell > lapseThresh) {
+                        cellClass = "lapse";
+                      }
+                      return <td key={idx} className={cellClass}>{cell}</td>;
+                    })}
+                  </tr>;
+                }
+              )}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan={cols} className="footer">
+                    Times in ms. 'FS' means false start.
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={cols} className="footer">
+                    Valid window: {validThresh} - {lapseThresh}
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+            </div>;
+  } catch(err) {
+    return <div> {err.message} </div>  ;       
+  }
+  
+}
+
+
+
+//-------------------------------------------------------
+class Old_DataTable extends React.Component {
   generateRows() {
     const results = this.props.results;
     const lapseThresh = this.props.lapseThresh;
