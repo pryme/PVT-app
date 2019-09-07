dc7ee29137be2fc79a2d0bfe2a98797cc39fd6c9
pryme <pryme4gh@gmail.com>
2019-09-07 14:41:59 -0400

    Refactor TestFooter into functional component.

--- src/test_footer.js
+++ src/test_footer.js
@@ -1,53 +1,48 @@
 import React from 'react';
 import Button from './button';
 
-class TestFooter extends React.Component {
-   constructor(props) {
-       super(props);
-   } 
+function TestFooter(props) {
+  switch (props.testStage) {
+    case "get_user":
+      return null;
+      break;
+    case "header":
+      return(
+          <div>
+              <Button name="Reset" onClick={props.resetAllCB}/>
+              <Button name="Settings" onClick={props.showSettingsCB}/>
+          </div>
+      );
+      break;
+    case "ready":
+      return(
+          <div>
+              <Button name="Reset" onClick={props.resetAllCB}/>
+          </div>
+      );
+      break;
+    case "running":
+      return null;
+      break;
+    case "done":
+      return(
+          <div>
+              <Button name="Reset" onClick={props.resetAllCB}/>
+          </div>
+      );
+      break;
+    case "settings":
+      return(
+          <div>
+              <Button name="Done with Settings" 
+              onClick={props.showSettingsCB}/>
+          </div>
+      );
+      break;
+    default:
+      return null;
+  }
 
-   render() {
-    switch (this.props.testStage) {
-      case "get_user":
-        return null;
-        break;
-      case "header":
-        return(
-            <div>
-                <Button name="Reset" onClick={this.props.resetAllCB}/>
-                <Button name="Settings" onClick={this.props.showSettingsCB}/>
-            </div>
-        );
-        break;
-      case "ready":
-        return(
-            <div>
-                <Button name="Reset" onClick={this.props.resetAllCB}/>
-            </div>
-        );
-        break;
-      case "running":
-        return null;
-        break;
-      case "done":
-        return(
-            <div>
-                <Button name="Reset" onClick={this.props.resetAllCB}/>
-            </div>
-        );
-        break;
-      case "settings":
-        return(
-            <div>
-                <Button name="Done with Settings" 
-                onClick={this.props.showSettingsCB}/>
-            </div>
-        );
-        break;
-      default:
-        return null;
-    }
-   }
 }
 
 export default TestFooter;
