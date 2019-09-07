c452e5b001f8fc042e2867e9764828f8487087cd
pryme <pryme4gh@gmail.com>
2019-09-07 11:15:15 -0400

    Clean up after refactoring.

--- src/response_timer.js
+++ src/response_timer.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 // following is not needed TODO 
-import StimulusDisplay from './stimulus_display';
+//import StimulusDisplay from './stimulus_display';
 
 /***************************************************
  * Refactoring this into a functional component using hooks.
@@ -80,81 +80,6 @@ function ResponseTimer(props) {
        {msg}
       </div>
     );
-  }
-}
-
-class OLD_ResponseTimer extends React.Component {
-  // measures time to respond to a stimulus that appears after random wait
-  constructor(props) {
-    super(props);
-    this.handleDisplayClick = this.handleDisplayClick.bind(this);
-    this.state = {
-      stimulusStart: null,  // when stimulus starts
-      msElapsed: null,  // milliseconds
-      clickEnabled: true  // whether displayClick is active or not
-    };
-  }
-  
-  componentDidMount() {
-    // wait a random interval, then start ticking
-    //const wait = 1000 * (2 + Math.floor(Math.random() * 8));  // milliseconds
-    const wait = 1000 * (2 + Math.floor(Math.random() * (this.props.maxWait - 2)));  // milliseconds
-    this.startID = setTimeout(
-        () => this.start(), wait
-      );
-    //document.getElementById("debug").innerHTML = "initial wait";  // TODO temp
-  }
-  
-  componentWillUnmount() {
-    clearTimeout(this.startID);  // kill timer if running
-    clearInterval(this.tickID);  // kill ticker
-    clearTimeout(this.cleanID);  // kill cleanup timer
-    this.props.cb(false, null);  // send ok to start signal
-  }
-  
-  start() {  
-    this.setState({
-      stimulusStart: new Date()  // capture start time for stimulus
-    });
-    // start counting
-    this.tickID = setInterval(
-      () => this.tick(), 5  // arrow fn doesn't define new 'this' value
-    );
-    //document.getElementById("debug").innerHTML = "ticking";  // TODO temp
-  }
-  
-  tick() {
-    const maxRT = 2000;  // milliseconds; timeout if RT is bigger than this
-    const d = new Date();
-    const elapsed = d.getTime() - this.state.stimulusStart.getTime()
-    this.setState({
-      msElapsed: elapsed
-    });
-    if (elapsed > maxRT) {
-      this.handleDisplayClick();
-    }
-  }
-  
-  handleDisplayClick() {
-    clearInterval(this.tickID);  // stop ticking
-    this.setState({clickEnabled: false});  // avoid double click issue
-    this.cleanID = setTimeout(
-      ( () => this.cleanUp() ), 1500
-    );
-  }
-  
-  cleanUp() {
-    this.setState({clickEnabled: true});  // re-activate displayClick
-    this.props.cb(true, this.state.msElapsed);  // send done signal and data
-  }
-  
-  render() {
-      return(<div id="response-timer">
-             <StimulusDisplay msElapsed={this.state.msElapsed} 
-               onClick={this.handleDisplayClick} >
-             </StimulusDisplay>
-             </div>
-            ); 
   }
 }
 
--- src/stimulus_display.js
+++ src/stimulus_display.js
@@ -20,21 +20,4 @@ function StimulusDisplay(props) {
   );
 }
 
-
-class OLD_StimulusDisplay extends React.Component {
-  // shows the stimulus counter
-  constructor(props) {
-    super(props);
-    
-  }
-  
-  render() {
-    return(
-           <div className="stimulus" onClick={this.props.onClick}>
-           <span className="stimulus-text">{this.props.msElapsed}</span>
-           </div> 
-          ); 
-  }
-}
-
 export default StimulusDisplay;
